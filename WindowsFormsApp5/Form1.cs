71684573dec90f3942ef4ccfc6eaa4e3bad9b893
Mahdi Hosseini <mahdidvb72@gmail.com>
2018-03-28 00:56:25 +0430

    c

--- WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Octokit;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,20 @@ namespace WindowsFormsApp5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+            var basicAuth = new Credentials("username", "password"); // NOTE: not real credentials
+            client.Credentials = basicAuth;
+            var createIssue = new NewIssue("this thing doesn't work");
+            createIssue.Body = "سلام";
+            createIssue.Labels.Add("bug");
+            createIssue.Labels.Add("wontfix");
+            createIssue.Labels.Add("Mahdi");
+            var issue = await client.Issue.Create("ghost1372", "WindowsFormsApp5", createIssue);
+
         }
     }
 }
