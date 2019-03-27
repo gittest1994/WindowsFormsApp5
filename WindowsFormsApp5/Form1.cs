770a79715468d303e3181b53d1184fe4dd124cee
Mahdi Hosseini <gittest1994@gmail.com>
2019-03-27 13:54:36 +0430

    add comment

--- WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/Form1.cs
@@ -18,6 +18,7 @@ namespace WindowsFormsApp5
             InitializeComponent();
         }
 
+        //Github Issue
         private async void Form1_Load(object sender, EventArgs e)
         {
             var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
